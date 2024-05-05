86814519fdf9d1185de2fa02050dc475ec9c2c70
lima1909 <linke.mario@web.de>
2024-05-05 16:32:33 +0200

    add parse headers to parser

--- lua/resty/parser.lua
+++ lua/resty/parser.lua
@@ -1,27 +1,40 @@
+---is a delimiter for a new starting rest call
 local delimiter = "###"
+
+---State-transitions: |init| -> |new| -> |call|
+local state_init = 0
+local state_new = 1
+local state_call = 2
 
 local parser = {}
 
 ---Parse the name after the start symbol '### [name]'
 ---@param line string
-local function parse_name(line)
+local function parse_name(line, nr)
 	local name = string.sub(line, #delimiter + 1)
 	name = vim.trim(name)
-	-- replace whitespaces with underscore
-	name = name:gsub("%s+", "_")
+
+	-- if no name set
+	if #name == 0 then
+		name = "noname-" .. nr
+	else
+		-- replace whitespaces with underscore
+		name = name:gsub("%s+", "_")
+	end
 
 	return name
 end
 
 ---Parse the rest call (method + url)
 ---@param line string
-local function parse_rest_call(line)
+local function parse_call(line)
 	local parts = vim.split(line, " ")
 	assert(#parts, 2, "expected two parts: method and url, got: " .. line)
 
 	local method = parts[1]:upper()
+	local url = parts[2]
 
-	return method, parts[2]
+	return method, url
 end
 
 ---Entry point, the parser
@@ -30,19 +43,33 @@ parser.parse = function(input)
 	local result = {}
 	local lines = input or ""
 	local name = ""
+	local current_state = state_init
 
 	for nr, line in ipairs(vim.split(lines, "\n")) do
+		-- start parsing a new request
 		if vim.startswith(line, delimiter) then
-			name = parse_name(line)
-			result[name] = { start_at = nr }
-		else
-			local method, url = parse_rest_call(line)
-			local current = result[name]
-			current["method"] = method
-			current["url"] = url
+			current_state = state_new
+			name = parse_name(line, nr)
+			result[name] = { start_at = nr, header = {} }
+			goto continue
 		end
 
-		-- print(nr, line)
+		if current_state == state_new then
+			current_state = state_call
+			local method, url = parse_call(line)
+			result[name]["method"] = method
+			result[name]["url"] = url
+			goto continue
+		end
+
+		if current_state == state_call then
+			-- set headers
+			if line:find(":") then
+				table.insert(result[name].header, vim.trim(line))
+			end
+		end
+
+		::continue::
 	end
 
 	return result
--- tests/parse_spec.lua
+++ tests/parse_spec.lua
@@ -3,8 +3,14 @@ describe("parser", function()
 	local assert = require("luassert")
 
 	it("parse one", function()
-		local input = [[### first test
-Get https://httpbin.org/get ]]
+		local input = [[
+### first test
+Get https://httpbin.org/get 
+
+accept: application/json  
+Authorization: Bearer mytoken123
+
+]]
 
 		local result = p.parse(input)
 		print(vim.inspect(result))
@@ -12,6 +18,7 @@ Get https://httpbin.org/get ]]
 			start_at = 1,
 			method = "GET",
 			url = "https://httpbin.org/get",
+			header = { "accept: application/json", "Authorization: Bearer mytoken123" },
 		})
 	end)
 end)

96b994e775bf073459b40c1d4f1f36ec1f089d6a
lima1909 <linke.mario@web.de>
2024-05-07 19:04:48 +0200

    extend the parser and add more tests

--- lua/resty/parser.lua
+++ lua/resty/parser.lua
@@ -1,27 +1,28 @@
----is a delimiter for a new starting rest call
-local delimiter = "###"
+---is a start_token for a new starting rest call
+local start_token = "###"
+---end_token is optional, is it necessary if the buffer contains more rows
+---otherwise is the end of the buffer the end of parsing
+local end_token = "---"
 
 ---State-transitions: |init| -> |new| -> |call|
 local state_init = 0
-local state_new = 1
-local state_call = 2
+local state_started = 1
+local state_ongoing = 2
 
 local parser = {}
 
 ---Parse the name after the start symbol '### [name]'
 ---@param line string
 local function parse_name(line, nr)
-	local name = string.sub(line, #delimiter + 1)
+	local name = string.sub(line, #start_token + 1)
 	name = vim.trim(name)
-
 	-- if no name set
 	if #name == 0 then
-		name = "noname-" .. nr
+		name = "noname_" .. nr
 	else
 		-- replace whitespaces with underscore
 		name = name:gsub("%s+", "_")
 	end
-
 	return name
 end
 
@@ -30,43 +31,44 @@ end
 local function parse_call(line)
 	local parts = vim.split(line, " ")
 	assert(#parts, 2, "expected two parts: method and url, got: " .. line)
-
 	local method = parts[1]:upper()
 	local url = parts[2]
-
 	return method, url
 end
 
 ---Entry point, the parser
----@param input string
+---@param input string | { }
 parser.parse = function(input)
+	local lines
+	if type(input) == "string" then
+		lines = vim.split(input, "\n")
+	elseif type(input) == "table" then
+		lines = input
+	end
+
 	local result = {}
-	local lines = input or ""
 	local name = ""
 	local current_state = state_init
 
-	for nr, line in ipairs(vim.split(lines, "\n")) do
-
+	for nr, line in ipairs(lines) do
 		-- start parsing a new request and parse the name
-		if vim.startswith(line, delimiter) then
-			current_state = state_new
-
+		if vim.startswith(line, start_token) then
+			current_state = state_started
 			name = parse_name(line, nr)
 			result[name] = { start_at = nr, end_at = nr, headers = {}, query = {} }
 			goto continue
 		end
 
 		-- parse method and url
-		if current_state == state_new then
-			current_state = state_call
-
+		if current_state == state_started then
+			current_state = state_ongoing
 			local method, url = parse_call(line)
 			result[name]["method"] = method
 			result[name]["url"] = url
 			goto continue
 		end
 
-		if current_state == state_call then
+		if current_state == state_ongoing then
 			-- set headers
 			if line:find(":") then
 				local parts = vim.split(line, ":")
@@ -75,7 +77,6 @@ parser.parse = function(input)
 				local value = vim.trim(parts[2])
 				result[name].headers[key] = value
 			end
-
 			-- set query
 			local pos_eq = line:find("=")
 			if pos_eq then
@@ -85,12 +86,16 @@ parser.parse = function(input)
 			end
 		end
 
-		result[name].end_at = nr
+		if result[name] then
+			result[name].end_at = nr
+		end
+
+		if vim.startswith(line, end_token) then
+			break
+		end
 
 		::continue::
 	end
-
 	return result
 end
-
 return parser
--- tests/parse_spec.lua
+++ tests/parse_spec.lua
@@ -31,4 +31,46 @@ include = sub, *
 			},
 		})
 	end)
+
+	it("no name request", function()
+		local input = [[
+### 
+Get https://httpbin.org/get 
+
+]]
+
+		local result = p.parse(input)
+		assert.are.same(result.noname_1, {
+			start_at = 1,
+			end_at = 4,
+			method = "GET",
+			url = "https://httpbin.org/get",
+			headers = {},
+			query = {},
+		})
+	end)
+
+	it("more chars after the request", function()
+		local input = [[
+### req
+Get https://httpbin.org/get 
+
+---
+
+head:val
+
+query=val
+
+]]
+
+		local result = p.parse(input)
+		assert.are.same(result.req, {
+			start_at = 1,
+			end_at = 4,
+			method = "GET",
+			url = "https://httpbin.org/get",
+			headers = {},
+			query = {},
+		})
+	end)
 end)

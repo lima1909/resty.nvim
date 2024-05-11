44d0c875a8a8895c1b0702d88958ff0e6f9aa3b8
lima1909 <linke.mario@web.de>
2024-05-11 20:10:28 +0200

    better output, with request name and start and end positions

--- lua/resty/init.lua
+++ lua/resty/init.lua
@@ -5,18 +5,27 @@ local M = {}
 
 _Last_req_def = nil
 
-local print_response_to_new_buf = function(response)
+local print_response_to_new_buf = function(req_def, response)
 	local buf = vim.api.nvim_create_buf(true, true)
 	-- vim.api.nvim_buf_set_name(buf, "Resty.http")
 	vim.api.nvim_set_option_value("filetype", "http", { buf = buf })
 
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "STATUS: " .. response.status })
-	vim.api.nvim_buf_set_lines(buf, 1, -1, false, response.headers)
+	vim.api.nvim_buf_set_lines(
+		buf,
+		0,
+		-1,
+		false,
+		{ "Request: " .. req_def.name .. " [" .. req_def.start_at .. " - " .. req_def.end_at .. "]" }
+	)
+	vim.api.nvim_buf_set_lines(buf, -1, -1, false, { "Response state: " .. response.status, "" })
+	vim.api.nvim_buf_set_lines(buf, -1, -1, false, { "" })
+	vim.api.nvim_buf_set_lines(buf, -1, -1, false, response.headers)
+	vim.api.nvim_buf_set_lines(buf, -1, -1, false, { "" })
 
 	local body = vim.split(response.body, "\n")
-	local line_nr = vim.api.nvim_buf_line_count(buf)
+	-- local line_nr = vim.api.nvim_buf_line_count(buf)
 	for i, r in ipairs(body) do
-		vim.api.nvim_buf_set_lines(buf, line_nr + i, -1, false, { r })
+		vim.api.nvim_buf_set_lines(buf, -1, -1, false, { r })
 	end
 
 	vim.api.nvim_win_set_buf(0, buf)
@@ -26,7 +35,7 @@ end
 M.last = function()
 	if _Last_req_def then
 		local response = curl.request(_Last_req_def.req)
-		print_response_to_new_buf(response)
+		print_response_to_new_buf(_Last_req_def, response)
 	else
 		error("No last request found. Run first [Resty run]")
 	end
@@ -52,7 +61,7 @@ M.run = function()
 	local response = curl.request(def.req)
 	_Last_req_def = def
 
-	print_response_to_new_buf(response)
+	print_response_to_new_buf(def, response)
 end
 
 return M

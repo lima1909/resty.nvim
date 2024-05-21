70580e10d21e055a881b08b3da75760c927959f9
lima1909 <linke.mario@web.de>
2024-05-21 17:15:36 +0200

    better exec and tests of jq

--- tests/exec_spec.lua
+++ tests/exec_spec.lua
@@ -1,56 +1,43 @@
 local assert = require("luassert")
+local stub = require("luassert.stub")
 local e = require("resty.exec")
 
 describe("jq:", function()
-	local result
-	local function get_output(output)
-		result = output
+	local mock_set_lines = stub.new(vim.api, "nvim_buf_set_lines")
+
+	local bufnr
+	local output
+
+	mock_set_lines.invokes(function(buffer_nr, _, _, _, content)
+		bufnr = buffer_nr
+		output = content
+		return 0
+	end)
+
+	local function jq_with_wait(bnr, json, jq_filter)
+		e.jq(bnr, json, jq_filter)
+		vim.wait(15, function()
+			return false
+		end)
 	end
 
 	it("with default filter", function()
-		local j = e.__jq(get_output, '{"value":true}')
-		j:start()
-		j:wait()
-
-		assert.are.same({ "{", '  "value": true', "}" }, result)
+		jq_with_wait(1, '{"value":true}')
+		assert.are.same(1, bufnr)
+		assert.are.same({ "{", '  "value": true', "}" }, output)
 	end)
 
 	it("with filter: .value", function()
-		local j = e.__jq(get_output, '{"value":true}', ".value")
-		j:start()
-		j:wait()
-
-		assert.are.same({ "true" }, result)
+		jq_with_wait(1, '{"value":true}', ".value")
+		assert.are.same(1, bufnr)
+		assert.are.same({ "true" }, output)
 	end)
 
 	it("error in json", function()
-		local j = e.__jq(get_output, '{"value":')
-		j:start()
-		j:wait()
-
-		assert.are.same({ "ERROR:", "", "jq: parse error: Unfinished JSON term at EOF at line 1, column 9" }, result)
-	end)
-
-	local stub = require("luassert.stub")
-
-	it("write to buffer with mock", function()
-		local mock_set_lines = stub.new(vim.api, "nvim_buf_set_lines")
-
-		local bufnr
-		local output
-
-		mock_set_lines.invokes(function(buffer_nr, _, _, _, content)
-			bufnr = buffer_nr
-			output = content
-			return 0
-		end)
-
-		e.jq(42, '{"value": 142}', ".value")
-		vim.wait(100, function()
-			return false
-		end)
-
-		assert.are.same(42, bufnr)
-		assert.are.same({ "142" }, output)
+		jq_with_wait(1, '{"value":')
+		assert.are.same(1, bufnr)
+		assert(output[1]:find("ERROR:"), output[1])
+		assert(output[2]:find(""), output[2])
+		assert(output[3]:find("Unfinished JSON term at EOF at line 1, column 9"), output[3])
 	end)
 end)

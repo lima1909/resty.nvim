2ac824cebd457648b6be035da84cb57c3b389791
lima1909 <linke.mario@web.de>
2024-05-21 20:17:36 +0200

    tidy up, remove globals

--- lua/resty/output.lua
+++ lua/resty/output.lua
@@ -1,13 +1,9 @@
 local exec = require("resty.exec")
 
-local default_output = "response"
-
 local M = {}
 
-local _response
-
 local function get_or_create_bufnr(name)
-	local output = name or default_output
+	local output = name or "response"
 	local bufnr = nil
 
 	for _, id in ipairs(vim.api.nvim_list_bufs()) do
@@ -48,7 +44,6 @@ end
 local function show_win(bufnr)
 	local winnr = win_exist(bufnr)
 	if not winnr then
-		-- vim.api.nvim_win_close(winnr, true)
 		vim.cmd("vsplit")
 		vim.cmd(string.format("buffer %d", bufnr))
 		vim.cmd("wincmd r")
@@ -64,7 +59,6 @@ end
 M.show_response = function(req_def, response, duration)
 	local bufnr = get_or_create_bufnr()
 	show_win(bufnr)
-	_response = response
 
 	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {
 		"Request: "
@@ -90,31 +84,31 @@ M.show_response = function(req_def, response, duration)
 
 	vim.api.nvim_win_set_buf(0, bufnr)
 	vim.api.nvim_win_set_cursor(0, { 1, 0 })
+
+	-- add key-mapping for using jq for the json-body
+	-- ----------------------------------------------
+	vim.keymap.set("n", "f", function()
+		local json = response.body --get_buf_context(bufnr)
+		exec.jq(bufnr, json)
+	end, {
+		silent = true,
+		buffer = bufnr,
+		desc = "format the json output with jq",
+	})
+
+	vim.keymap.set("n", "ff", function()
+		local jq_filter = vim.fn.input("Filter: ")
+		if jq_filter == "" then
+			return
+		end
+
+		local json = response.body --get_buf_context(bufnr)
+		exec.jq(bufnr, json, jq_filter)
+	end, {
+		silent = true,
+		buffer = bufnr,
+		desc = "format the json output with jq with a given query",
+	})
 end
-
-local bufnr = get_or_create_bufnr()
-
-vim.keymap.set("n", "f", function()
-	local json = _response.body --get_buf_context(bufnr)
-	exec.jq(bufnr, json)
-end, {
-	silent = true,
-	buffer = bufnr,
-	desc = "format the json output with jq",
-})
-
-vim.keymap.set("n", "ff", function()
-	local jq_filter = vim.fn.input("Filter: ")
-	if jq_filter == "" then
-		return
-	end
-
-	local json = _response.body --get_buf_context(bufnr)
-	exec.jq(bufnr, json, jq_filter)
-end, {
-	silent = true,
-	buffer = bufnr,
-	desc = "format the json output with jq with a given query",
-})
 
 return M

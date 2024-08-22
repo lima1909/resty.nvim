43ce340071621d5cb85d6472ea5c29548410e27f
lima1909 <linke.mario@web.de>
2024-08-22 19:51:42 +0200

    add telescope to minimal init for git pipeline

--- spec/minimal_init.lua
+++ spec/minimal_init.lua
@@ -1,5 +1,5 @@
+-- git checkout plenary
 local plenary_dir = "/tmp/plenary.nvim"
-
 if vim.fn.isdirectory(plenary_dir) == 0 then
 	vim.fn.system({
 		"git",
@@ -11,8 +11,25 @@ if vim.fn.isdirectory(plenary_dir) == 0 then
 	})
 end
 
+-- git checkout telescope
+local telescope_dir = "/tmp/telescope.nvim"
+if vim.fn.isdirectory(telescope_dir) == 0 then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/nvim-telescope/telescope.nvim",
+		telescope_dir,
+	})
+end
+
 vim.opt.rtp:append(".")
 vim.opt.rtp:append(plenary_dir)
+vim.opt.rtp:append(telescope_dir)
 
 vim.cmd.runtime({ "plugin/plenary.vim", bang = true })
+vim.cmd.runtime({ "plugin/telescope.vim", bang = true })
+
 require("plenary.busted")
+require("telescope")

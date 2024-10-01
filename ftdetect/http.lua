d05acc264855a00444f56fe706ae9322b8b08ec8
lima1909 <linke.mario@web.de>
2024-10-01 10:57:48 +0200

    add an idea for diagnostic

--- ftdetect/http.lua
+++ ftdetect/http.lua
@@ -18,7 +18,7 @@ vim.keymap.set("n", "-", "zc")
 
 -- Reset diagnostic by changing the file
 vim.api.nvim_create_augroup("RestyDiagnostic", { clear = true })
--- text change in Insert and Normal mode
+-- text change in Insert and Normal mode => diagnostic reset
 vim.api.nvim_create_autocmd({ "TextChangedI", "TextChanged" }, {
 	group = "RestyDiagnostic",
 	pattern = "*.http",
@@ -26,3 +26,17 @@ vim.api.nvim_create_autocmd({ "TextChangedI", "TextChanged" }, {
 		require("resty.diagnostic").reset(ev.buf)
 	end,
 })
+-- text change in Esc and Normal mode => diagnostic on
+-- vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+-- 	group = "RestyDiagnostic",
+-- 	pattern = "*.http",
+-- 	callback = function(ev)
+-- 		local winnr = vim.api.nvim_get_current_win()
+-- 		local row = vim.api.nvim_win_get_cursor(winnr)[1]
+-- 		local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+--
+-- 		-- local r = require("resty.parser.parserng").parse(lines, row)
+-- 		local r = require("resty.parser").parse(lines, row)
+-- 		require("resty.diagnostic").show(ev.buf, r)
+-- 	end,
+-- })

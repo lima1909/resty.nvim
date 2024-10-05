2eb33e7a36c196d4afa82c650c3e6c92e45015c4
Nuno Ramos <nuno.m.ramos@tecnico.ulisboa.pt>
2024-10-05 22:34:13 +0100

    removed fold related keymaps

--- ftdetect/http.lua
+++ ftdetect/http.lua
@@ -1,21 +1,5 @@
 vim.filetype.add({ extension = { http = "http" } })
 
-vim.keymap.set("n", "zz", function()
-	-- toggle folding
-	if vim.opt.foldmethod._value ~= "expr" then
-		vim.cmd("setlocal foldmethod=expr")
-		vim.cmd("setlocal foldexpr=v:lua.foldexpr(v:lnum)")
-	else
-		vim.cmd("setlocal foldmethod=manual")
-		vim.cmd("normal zE")
-	end
-
-	-- vim.cmd("setlocal foldtext=~~~")
-end, { silent = true, desc = "[zz] activate folding" })
-
-vim.keymap.set("n", "+", "zo")
-vim.keymap.set("n", "-", "zc")
-
 -- Reset diagnostic by changing the file
 vim.api.nvim_create_augroup("RestyDiagnostic", { clear = true })
 -- text change in Insert and Normal mode => diagnostic reset

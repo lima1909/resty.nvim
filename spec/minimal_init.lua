caf777aae1dd354132f8fc580a5852c3f3bf3388
lima1909 <linke.mario@web.de>
2024-10-22 18:20:07 +0200

    deactivate telescoope and nvim-cmp in minimal_init.lua

--- spec/minimal_init.lua
+++ spec/minimal_init.lua
@@ -12,40 +12,40 @@ if vim.fn.isdirectory(plenary_dir) == 0 then
 end
 
 -- git checkout telescope
-local telescope_dir = "/tmp/telescope.nvim"
-if vim.fn.isdirectory(telescope_dir) == 0 then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/nvim-telescope/telescope.nvim",
-		telescope_dir,
-	})
-end
+-- local telescope_dir = "/tmp/telescope.nvim"
+-- if vim.fn.isdirectory(telescope_dir) == 0 then
+-- 	vim.fn.system({
+-- 		"git",
+-- 		"clone",
+-- 		"--depth",
+-- 		"1",
+-- 		"https://github.com/nvim-telescope/telescope.nvim",
+-- 		telescope_dir,
+-- 	})
+-- end
 
--- git checkout telescope
-local cmp_dir = "/tmp/nvim-cmp"
-if vim.fn.isdirectory(cmp_dir) == 0 then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/hrsh7th/nvim-cmp.git",
-		cmp_dir,
-	})
-end
+-- git checkout nvim-cmp
+-- local cmp_dir = "/tmp/nvim-cmp"
+-- if vim.fn.isdirectory(cmp_dir) == 0 then
+-- 	vim.fn.system({
+-- 		"git",
+-- 		"clone",
+-- 		"--depth",
+-- 		"1",
+-- 		"https://github.com/hrsh7th/nvim-cmp.git",
+-- 		cmp_dir,
+-- 	})
+-- end
 
 vim.opt.rtp:append(".")
 vim.opt.rtp:append(plenary_dir)
-vim.opt.rtp:append(telescope_dir)
-vim.opt.rtp:append(cmp_dir)
+-- vim.opt.rtp:append(telescope_dir)
+-- vim.opt.rtp:append(cmp_dir)
 
 vim.cmd.runtime({ "plugin/plenary.vim", bang = true })
-vim.cmd.runtime({ "plugin/telescope.vim", bang = true })
-vim.cmd.runtime({ "plugin/nvim-cmp", bang = true })
+-- vim.cmd.runtime({ "plugin/telescope.vim", bang = true })
+-- vim.cmd.runtime({ "plugin/nvim-cmp", bang = true })
 
 require("plenary.busted")
-require("telescope")
-require("cmp")
+-- require("telescope")
+-- require("cmp")

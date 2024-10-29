3863664668b5360f32d5793d166a6bdf9fc082cb
lima1909 <linke.mario@web.de>
2024-10-29 17:00:15 +0100

    add cmp-setup filetypes for resty and http

--- after/plugin/resty_http_cmp.lua
+++ after/plugin/resty_http_cmp.lua
@@ -2,4 +2,11 @@ local ok, cmp = pcall(require, "cmp")
 if ok then
 	-- add resty completion, if nvim-cmp is installed
 	cmp.register_source("resty", require("resty.extension.resty-cmp").new())
+	cmp.setup.filetype({ "resty", "http" }, {
+		sources = cmp.config.sources({
+			{ name = "resty" },
+			{ name = "buffer" },
+			{ name = "text" },
+		}),
+	})
 end
